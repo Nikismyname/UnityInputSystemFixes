5f0608ba3da119630d1b29444c6ad62b24d6cb0e
Nikismyname <nikolaysvlpopov@gmail.com>
2022-07-23 15:28:35 +0300

    Fixing the mod problem for use1 use2 use3

--- Assets/InputSystem/StarterAssetsInputs.cs
+++ Assets/InputSystem/StarterAssetsInputs.cs
@@ -5,8 +5,16 @@ using UnityEngine.InputSystem;
 
 namespace StarterAssets
 {
+	using System.Collections.Generic;
+	using System.Linq;
+
 	public class StarterAssetsInputs : MonoBehaviour
 	{
+		private const string ANY_PATH = "<Keyboard>/anyKey";
+		private const string MOD_PREFIX = "modifier";
+		
+		[SerializeField] private InputActionAsset mapAsset;
+		
 		[Header("Character Input Values")]
 		public Vector2 move;
 		public Vector2 look;
@@ -20,6 +28,40 @@ namespace StarterAssets
 		public bool cursorLocked = true;
 		public bool cursorInputForLook = true;
 
+		private InputActionMap map;
+
+		private List<InputActionBundle> allBundles;
+		private InputActionBundle onUse1;
+		private InputActionBundle onUse2;
+		private InputActionBundle onUse3;
+			
+		private void Start()
+		{
+			this.map = this.mapAsset.actionMaps[0];
+			this.onUse1 = this.ParseInput("Use1");
+			this.onUse2 = this.ParseInput("Use2");
+			this.onUse3 = this.ParseInput("Use3");
+			this.allBundles = new List<InputActionBundle>() { this.onUse1, this.onUse2, this.onUse3 };
+		}
+
+		public bool Use1
+		{
+			get => this.onUse1.Triggered && !this.CheckForCollision(this.onUse1);
+			set => this.onUse1.Triggered = value;
+		}
+
+		public bool Use2
+		{
+			get => this.onUse2.Triggered && !this.CheckForCollision(this.onUse2);
+			set => this.onUse2.Triggered = value;
+		}
+        
+		public bool Use3
+		{
+			get => this.onUse3.Triggered && !this.CheckForCollision(this.onUse3);
+			set => this.onUse3.Triggered = value;
+		}
+		
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 		public void OnMove(InputValue value)
 		{
@@ -43,8 +85,16 @@ namespace StarterAssets
 		{
 			SprintInput(value.isPressed);
 		}
-#endif
 
+		// no mods
+		public void OnUse1(InputValue value) => this.Use1 = value.isPressed;
+		
+		// one mod - shift
+		public void OnUse2(InputValue value) => this.Use2 = value.isPressed;
+		
+		// two mods - shift + ctrl
+		public void OnUse3(InputValue value) => this.Use3 = value.isPressed;
+#endif
 
 		public void MoveInput(Vector2 newMoveDirection)
 		{
@@ -75,6 +125,48 @@ namespace StarterAssets
 		{
 			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
 		}
+		
+		// counts the non empty modifiers
+		private InputActionBundle ParseInput(string key)
+		{
+			InputAction onUseInput = this.map.actions.Single(x => x.name == key);
+			
+			int count = 0;
+			foreach (InputBinding item in onUseInput.bindings)
+				if (item.name.StartsWith(MOD_PREFIX) && item.path != ANY_PATH) 
+					count++;
+
+			InputActionBundle result = new()
+			{
+				NonAnyMods = count,
+				Triggered = false,
+			};
+            
+			return result;
+		}
+		
+		// checks that there is not another action (currently triggered) with more mods that the one being evaluated.
+		private bool CheckForCollision(InputActionBundle bundleIn)
+		{
+			foreach (InputActionBundle otherBundle in this.allBundles)
+			{
+				if (otherBundle == bundleIn) continue;
+				if (otherBundle.Triggered == false) continue;
+                
+				if (otherBundle.NonAnyMods > bundleIn.NonAnyMods)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
 	}
 	
+	public class InputActionBundle
+	{
+		public int  NonAnyMods { get; set; }
+
+		public bool Triggered { get; set; }
+	}
 }
